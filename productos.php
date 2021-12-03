68489f86810d27629430b36d2e786972b8f40344
Josue Garza <A281876@alumnos.uaslp.mx>
2021-12-03 00:43:08 -0600

    holy sensations spa v16-1
    quitamos coments

--- productos.php
+++ productos.php
@@ -55,33 +55,5 @@ $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
     </div>
 <?php  } ?>
 
-
-
-
-
-<!-- card 2
-<div class="col-md-4">
-    <div class="card">
-        <img class="card-img-top" src="holder.js/100x180/" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <p class="card-text">Text</p>
-        </div>
-    </div>
-</div>
-
-<div class="col-md-4">
-    <div class="card">
-        <img class="card-img-top" src="holder.js/100x180/" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <p class="card-text">Text</p>
-        </div>
-    </div>
-</div> -->
-
-
-
-
 <?php include("template/footer.php")
 ?>
