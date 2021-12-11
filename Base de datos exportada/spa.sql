c50336e537c1c550630529ede17dfc931c652f55
Josue Garza <A281876@alumnos.uaslp.mx>
2021-12-10 19:20:56 -0600

    holy sensations spa v30-2
    base actualizada

---
+++ Base de datos exportada/spa.sql
@@ -0,0 +1,326 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-12-2021 a las 01:57:37
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `spa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrito`
+--
+
+CREATE TABLE `carrito` (
+  `idUsuario` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `idProducto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `carrito`
+--
+
+INSERT INTO `carrito` (`idUsuario`, `Cantidad`, `idProducto`) VALUES
+(4, 3, 34);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE `curso` (
+  `idCurso` int(11) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Descripcion` varchar(500) NOT NULL,
+  `Duracion` varchar(100) NOT NULL,
+  `Fecha` varchar(100) NOT NULL,
+  `Cupo` varchar(50) NOT NULL,
+  `Imagen` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `curso`
+--
+
+INSERT INTO `curso` (`idCurso`, `Nombre`, `Descripcion`, `Duracion`, `Fecha`, `Cupo`, `Imagen`) VALUES
+(5, '  Curso de masaje español', '  Curso donde aprendera la tecnica de masaje español.', '  12 semanas', '  12/01/22 - 12/03/22', '  6 personas', '1638838789_curso de masajes español.png'),
+(6, '    Curso de masaje drenaje linfatico', '    Curso donde aprenderá la técnica de drenaje linfático.', '    12 semanas', '    12/01/22 - 12/03/22', '90 personas', '1638838844_curso de masajes drenaje.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `informacion`
+--
+
+CREATE TABLE `informacion` (
+  `idCurso` int(11) NOT NULL,
+  `correo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `informacion`
+--
+
+INSERT INTO `informacion` (`idCurso`, `correo`) VALUES
+(5, 'negociosgarzas@gmail.com'),
+(5, 'negociosgarzas@gmail.com'),
+(5, 'profeedgar@gmail.pasemeporfavor'),
+(6, 'profeedgar@gmail.pasemeporfavor');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idProducto` int(11) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Descripcion` varchar(200) NOT NULL,
+  `Precio` float NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `Categoria` varchar(50) NOT NULL,
+  `Imagen` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`idProducto`, `Nombre`, `Descripcion`, `Precio`, `Cantidad`, `Categoria`, `Imagen`) VALUES
+(34, '  Crema ', '  Crema para manos y cara', 159, 9, ' Crema', '1638839923_crema.jfif'),
+(35, '  Desodorante', '  Desodorante con olor fresco que te hara sentir fresco todo el dia', 69, 4, '  Deshodorante', '1638839803_desodorante.jfif'),
+(36, 'Mascarilla', 'Mascarillas antiacné ', 100, 9, 'Mascarillas', '1638839963_mascarillas.jfif'),
+(37, ' crema 2', '  crema para barros de chavos', 99, 5, ' cremas', '1639084951_Faciales.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `promocion`
+--
+
+CREATE TABLE `promocion` (
+  `idPromocion` int(11) NOT NULL,
+  `Nombre` varchar(200) NOT NULL,
+  `Descripcion` varchar(500) NOT NULL,
+  `Precio` float NOT NULL,
+  `Duracion` varchar(100) NOT NULL,
+  `Imagen` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `promocion`
+--
+
+INSERT INTO `promocion` (`idPromocion`, `Nombre`, `Descripcion`, `Precio`, `Duracion`, `Imagen`) VALUES
+(4, 'Paquete amigas', ' Por un precio especial las dos reciben un facial.', 699, ' 10/12/21 - 31/13/21', '1638838927_2.png'),
+(5, ' Paquete pareja', ' Ambos podran recibir un masaje por un precio especial.', 699, ' 10/12/21 - 31/13/21', '1638839082_1.png'),
+(6, ' Combo ', ' Limpieza facial + Masaje relajante + Reflexologia Podal', 650, ' 10/12/21 - 31/13/21', '1638839542_combo 1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `idRol` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`idRol`, `Nombre`) VALUES
+(1, 'General'),
+(2, 'Administrador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `idServicio` int(11) NOT NULL,
+  `Nombre` varchar(200) NOT NULL,
+  `Descripcion` varchar(500) NOT NULL,
+  `Precio` float NOT NULL,
+  `Duracion` varchar(50) NOT NULL,
+  `Imagen` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`idServicio`, `Nombre`, `Descripcion`, `Precio`, `Duracion`, `Imagen`) VALUES
+(1, ' Masaje reductivo', ' Es un masaje que ayuda a reducir de peso.', 350, ' 1 hora', '1638677284_Masaje reductivo.png'),
+(2, ' Masaje normal', ' Masaje que relajara los músculos y descontracturara los mismos.', 350, ' 1 hora', '1638648601_masajes.png'),
+(3, ' Faciales', ' Tratamiento facial para eliminar imperfecciones. ', 350, ' 1 hora', '1638839606_Faciales.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Pwd` varchar(50) NOT NULL,
+  `rePwd` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `idRol` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `Nombre`, `Pwd`, `rePwd`, `Email`, `Direccion`, `idRol`) VALUES
+(4, 'josue', '123', '123', 'yoshogarza42@gmail.com', 'Privada manuela lopez de lara #110-a colonia los reyitos', 2),
+(5, 'marco', '123', '123', 'yoshogarza42@gmail.com', 'Arbolitos #666', 1),
+(14, 'Profe edgar', '123', '123', 'profeedgar@gmail.com', 'Lomas 4 seccion ', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indices de la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`idCurso`);
+
+--
+-- Indices de la tabla `informacion`
+--
+ALTER TABLE `informacion`
+  ADD KEY `idCurso` (`idCurso`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idProducto`);
+
+--
+-- Indices de la tabla `promocion`
+--
+ALTER TABLE `promocion`
+  ADD PRIMARY KEY (`idPromocion`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`idRol`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`idServicio`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD KEY `idRol` (`idRol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `idCurso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT de la tabla `promocion`
+--
+ALTER TABLE `promocion`
+  MODIFY `idPromocion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `idRol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `idServicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD CONSTRAINT `carrito_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`),
+  ADD CONSTRAINT `carrito_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`);
+
+--
+-- Filtros para la tabla `informacion`
+--
+ALTER TABLE `informacion`
+  ADD CONSTRAINT `informacion_ibfk_1` FOREIGN KEY (`idCurso`) REFERENCES `curso` (`idCurso`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`idRol`) REFERENCES `roles` (`idRol`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
